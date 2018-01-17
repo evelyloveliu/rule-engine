2f6693abf40848f20d301e2e886c5d457d796449
liuchy <liuchy@fenqi.im>
2018-01-17 13:45:44 +0800

    refine code

--- src/main/java/com/liuchy/controller/RuleController.java
+++ src/main/java/com/liuchy/controller/RuleController.java
@@ -1,12 +1,12 @@
 package com.liuchy.controller;
 
 import com.liuchy.domain.Person;
+import com.liuchy.service.RuleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.liuchy.service.RuleService;
 
 /**
  * Created by liuchy on 2018/1/16.
@@ -18,7 +18,7 @@ public class RuleController {
     private RuleService ruleService;
 
     @RequestMapping(value = "/verify", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public String ruleVerify(@RequestParam(value = "data") Person person) {
+    public String ruleVerify(@RequestBody Person person) {
         ruleService.getKieSession(person);
         return "11111";
     }
